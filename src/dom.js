7f43b90478a49fa899fe2b688869f18878a77e5f
shannqa@gmail.com <shannqa@gmail.com>
2023-11-08 19:16:19 +0100

    Add arrow images

--- src/dom.js
+++ src/dom.js
@@ -6,17 +6,39 @@ function createDom() {
   
   createGrid();
   
+  const arrowUpSrc = "./arrow_upward_FILL0_wght400_GRAD0_opsz24.svg";
+const arrowLeftSrc = "./arrow_back_FILL0_wght400_GRAD0_opsz24.svg";
+const arrowRightSrc = "./arrow_forward_FILL0_wght400_GRAD0_opsz24.svg";
+const arrowDownSrc = "./arrow_downward_FILL0_wght400_GRAD0_opsz24.svg";
+
+const arrowUpImg = new Image();
+const arrowLeftImg = new Image();
+const arrowRightImg = new Image();
+const arrowDownImg = new Image();
+arrowUpImg.src = arrowUpSrc;
+arrowLeftImg.src = arrowLeftSrc;
+arrowRightImg.src = arrowRightSrc;
+arrowDownImg.src = arrowDownSrc;
+
+  
   const arrows = document.createElement("div");
   const upArrow = document.createElement("div");
   const leftArrow = document.createElement("div");
   const rightArrow = document.createElement("div");
   const downArrow = document.createElement("div");
 
-  upArrow.textContent = "UP";
-  leftArrow.textContent = "LFT";
-  rightArrow.textContent = "RGHT";
-  downArrow.textContent = "DWN";
-  
+upArrow.appendChild(arrowUpImg);
+leftArrow.appendChild(arrowLeftImg);
+rightArrow.appendChild(arrowRightImg);
+downArrow.appendChild(arrowDownImg);
+
+arrows.classList.add("arrows");
+upArrow.classList.add("arrow-up");
+leftArrow.classList.add("arrow-left");
+rightArrow.classList.add("arrow-right");
+downArrow.classList.add("arrow-down");
+
+
   upArrow.addEventListener("click", () => {
     move("up");
   });
